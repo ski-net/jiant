85250e5aed003d68b41c3677eca355ce79577efe
Adam Poliak <azpoliak@gmail.com>
2018-07-17 16:11:04 -0400

    added script to download data for edge-probing

---
+++ src/probing_data/get_subsentence_probing_data.sh
@@ -0,0 +1,13 @@
+wget http://decomp.net/wp-content/uploads/2015/08/protoroles_eng_pb.tar.gz
+tar zxvf protorole_eng_pb.tar.gz
+
+wget http://decomp.net/wp-content/uploads/2015/08/protoroles_eng_ud1.2.tar.gz
+tar zxvf protoroles_eng_ud1.2.tar.gz
+
+wget https://github.com/UniversalDependencies/UD_English/archive/r1.2.tar.gz
+tar -zxvf r1.2.tar.gz
+
+mkdir recast1
+cd recast1
+wget http://decomp.net/wp-content/uploads/2017/11/inference_is_everything.zip
+unzip inference_is_everything
