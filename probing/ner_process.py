7b042f4e136b9151f5ae7bfbfc15977151535eff
Berlin Chen <berlinchen7@gmail.com>
2018-07-19 20:54:37 +0000

    fix bugs in NER data processing

--- probing/ner_process.py
+++ probing/ner_process.py
@@ -1,11 +1,24 @@
 import json
 
 filepath = "/home/bchen6_swarthmore_edu/recast_ner_data.json"
+metafilepath = "/home/bchen6_swarthmore_edu/recast-ner-metadata.json"
+corpus = "groningen-meaning-bank"
+
+count = 0
+
+corpus_separator_index = 59970
 
 with open(filepath, 'r') as f:
     data = json.load(f)
 
+with open(metafilepath, 'r') as f:
+    metadata = json.load(f)
+
+data = data[:corpus_separator_index] if corpus == "CoNLL-2003" else data[corpus_separator_index:]
+metadata = metadata[:corpus_separator_index] if corpus == "CoNLL-2003" else metadata[corpus_separator_index:]
+
 def find_span(phrase, sentence):
+    global count
     split_sentence, split_phrase = sentence.split(), phrase.split()
     span_start = -1
     for i in range(len(split_sentence)- len(split_phrase) + 1):
@@ -18,6 +31,7 @@ def find_span(phrase, sentence):
             break
     if span_start == -1:
         print("Bad parsing.  Discounting target \"" + phrase + "\"")
+        count += 1
         #raise RuntimeError("Span not found.")
     return [span_start, span_start + len(split_phrase)]
 
@@ -25,12 +39,15 @@ train_data, test_data = [], []
 
 def json_to_json(split):
     interim_dict = {}
-    for datum in data:
+    for index, datum in enumerate(data):
         if datum["split"] != split or datum["binary-label"] == False:
             continue
         text = datum["context"]
         if text not in interim_dict:
-            interim_dict[text] = {'text': text, "targets": [], "info": {"sent-id": datum["sent-id"], "source": "NER"}}
+            info = {"source": corpus, "corpus-license": metadata[index]["corpus-license"], "corpus-sent-id": metadata[index]["corpus-sent-id"], \
+                                      "human-validated": metadata[index]["human-validated"], "misc": metadata[index]["misc"], \
+                                        "quality": metadata[index]["quality"]}
+            interim_dict[text] = {'text': text, "targets": [], "info":info}
         phrase, label = datum["hypothesis"].replace(" is an ", " is a ").split(" is a ")
         span1 = find_span(phrase, text)
         if span1[0] == -1:
@@ -38,14 +55,13 @@ def json_to_json(split):
         interim_dict[text]["targets"].append({"label": label, \
                             "span1": span1, "info":{}})
     clean_data_list = interim_dict.values()
-    with open('ner_' + split + ".json", 'w') as outfile:
+    with open(corpus + '_' + split + ".json", 'w') as outfile:
         for datum in clean_data_list:
             json.dump(datum, outfile)
             outfile.write("\n")
 
-
-
 json_to_json("train")
+json_to_json("dev")
 json_to_json("test")
 
-
+print(count)
